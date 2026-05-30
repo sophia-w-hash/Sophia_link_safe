dd66717ff141b757f7c29011d5f110f7b250c109
sophia-w-hash <sophia@wwkart.com>
2026-05-30 22:17:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 require("dotenv").config();
 
 const express = require("express");
+const emailRoutes = require("./routes/emailRoutes");
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send(`
-    <h1>Mailer API Running 🚀</h1>
-    <p>Server is working correctly.</p>
-  `);
+  res.send("Mailer API Running 🚀");
 });
 
-app.get("/health", (req, res) => {
-  res.json({
-    success: true,
-    status: "online"
-  });
-});
+app.use("/api/email", emailRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
