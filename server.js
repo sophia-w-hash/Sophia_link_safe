615432c5bdfdb4a51d0f728fb04b70780505b48f
sophia-w-hash <sophia@wwkart.com>
2026-05-31 12:59:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-require("dotenv").config();
-
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/",(req,res)=>{
-  res.sendFile(
-    path.join(__dirname,"public","index.html")
-  );
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
