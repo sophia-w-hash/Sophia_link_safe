1887bec98d1ce8587776fe15d556bcd12f67dc00
sophia-w-hash <sophia@wwkart.com>
2026-05-31 14:06:28 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,11 +1,65 @@
-async function login() {
-  const res = await fetch("/login", {
-    method: "POST"
+if (!localStorage.getItem("auth")) {
+  location.href = "/login.html";
+}
+
+document
+  .getElementById("logoutBtn")
+  .addEventListener("dblclick", () => {
+    localStorage.removeItem("auth");
+    location.href = "/login.html";
   });
 
-  const data = await res.json();
+document
+  .getElementById("sendBtn")
+  .addEventListener("click", sendMail);
 
-  if (data.success) {
-    window.location.href = "/launcher";
+async function sendMail() {
+
+  const btn =
+    document.getElementById("sendBtn");
+
+  btn.disabled = true;
+  btn.innerText = "Sending...";
+
+  try {
+
+    const response = await fetch("/send", {
+      method: "POST",
+      headers: {
+        "Content-Type":
+          "application/json"
+      },
+      body: JSON.stringify({
+        senderName:
+          document.getElementById("senderName").value,
+
+        gmail:
+          document.getElementById("gmail").value,
+
+        appPassword:
+          document.getElementById("appPassword").value,
+
+        recipient:
+          document.getElementById("recipient").value,
+
+        subject:
+          document.getElementById("subject").value,
+
+        message:
+          document.getElementById("message").value
+      })
+    });
+
+    const data = await response.json();
+
+    alert(data.message);
+
+  } catch {
+
+    alert("Error");
+
   }
+
+  btn.disabled = false;
+  btn.innerText = "Send";
 }
